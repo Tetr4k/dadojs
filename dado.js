0bae2810c8152688d95bfa7f5c6958c1226889da
Gabriel Ribeiro <g2012ribeiro@gmail.com>
2021-01-08 22:58:51 -0300

    melhorando dado.js

--- dado.js
+++ dado.js
@@ -1,98 +1,10 @@
-//n1 = 0;
+n1 = 0;
 
 function gera(){
     return Math.floor(Math.random() * 6)+1;
 }
 
-function monta(){
-
-    switch(gera()){
-
-        case 1:
-
-            document.getElementById("p1").style.background = "white";
-            document.getElementById("p2").style.background = "white";
-            document.getElementById("p3").style.background = "white";
-            document.getElementById("p5").style.background = "white";
-            document.getElementById("p6").style.background = "white";
-            document.getElementById("p7").style.background = "white";
-
-            document.getElementById("p4").style.background = "black";
-
-            break;
-
-        case 2:
-
-            document.getElementById("p1").style.background = "white";
-            document.getElementById("p3").style.background = "white";
-            document.getElementById("p4").style.background = "white";
-            document.getElementById("p5").style.background = "white";
-            document.getElementById("p7").style.background = "white";
-
-            document.getElementById("p2").style.background = "black";
-            document.getElementById("p6").style.background = "black";
-
-            break;
-
-        case 3:
-
-            document.getElementById("p2").style.background = "white";
-            document.getElementById("p3").style.background = "white";
-            document.getElementById("p5").style.background = "white";
-            document.getElementById("p6").style.background = "white";
-
-            document.getElementById("p1").style.background = "black";
-            document.getElementById("p4").style.background = "black";
-            document.getElementById("p7").style.background = "black";
-
-            break;
-
-        case 4:
-
-            document.getElementById("p3").style.background = "white";
-            document.getElementById("p4").style.background = "white";
-            document.getElementById("p5").style.background = "white";
-
-            document.getElementById("p1").style.background = "black";
-            document.getElementById("p2").style.background = "black";
-            document.getElementById("p6").style.background = "black";
-            document.getElementById("p7").style.background = "black";
-
-            break;
-
-        case 5:
-
-            document.getElementById("p3").style.background = "white";
-            document.getElementById("p5").style.background = "white";
-
-            document.getElementById("p1").style.background = "black";
-            document.getElementById("p2").style.background = "black";
-            document.getElementById("p4").style.background = "black";
-            document.getElementById("p6").style.background = "black";
-            document.getElementById("p7").style.background = "black";
-
-            break;
-
-        case 6:
-
-            document.getElementById("p4").style.background = "white";
-
-            document.getElementById("p1").style.background = "black";
-            document.getElementById("p2").style.background = "black";
-            document.getElementById("p3").style.background = "black";
-            document.getElementById("p5").style.background = "black";
-            document.getElementById("p6").style.background = "black";
-            document.getElementById("p7").style.background = "black";
-
-            break;
-
-    }
-
-}
-
 function geraNaoRepetido(){
-    /*
-    
     n2 = Math.floor(Math.random() * 6)+1;
 
     if(n2==n1){
@@ -102,6 +14,91 @@ function geraNaoRepetido(){
         n1=n2;
         return n2;
     }
+}
 
-    */
+const dado = document.getElementById("dado");
+dado.onclick = function (){
+//  switch(gera()){
+    switch(geraNaoRepetido()){
+
+        case 1:
+
+            document.getElementById("p1").style.backgroundColor = "white";
+            document.getElementById("p2").style.backgroundColor = "white";
+            document.getElementById("p3").style.backgroundColor = "white";
+            document.getElementById("p5").style.backgroundColor = "white";
+            document.getElementById("p6").style.backgroundColor = "white";
+            document.getElementById("p7").style.backgroundColor = "white";
+
+            document.getElementById("p4").style.backgroundColor = "black";
+
+            break;
+
+        case 2:
+
+            document.getElementById("p3").style.backgroundColor = "white";
+            document.getElementById("p1").style.backgroundColor = "white";
+            document.getElementById("p4").style.backgroundColor = "white";
+            document.getElementById("p5").style.backgroundColor = "white";
+            document.getElementById("p7").style.backgroundColor = "white";
+
+            document.getElementById("p6").style.backgroundColor = "black";
+            document.getElementById("p2").style.backgroundColor = "black";
+
+            break;
+
+        case 3:
+
+            document.getElementById("p2").style.backgroundColor = "white";
+            document.getElementById("p5").style.backgroundColor = "white";
+            document.getElementById("p6").style.backgroundColor = "white";
+            document.getElementById("p3").style.backgroundColor = "white";
+
+            document.getElementById("p4").style.backgroundColor = "black";
+            document.getElementById("p7").style.backgroundColor = "black";
+            document.getElementById("p1").style.backgroundColor = "black";
+
+            break;
+
+        case 4:
+
+            document.getElementById("p4").style.backgroundColor = "white";
+            document.getElementById("p3").style.backgroundColor = "white";
+            document.getElementById("p5").style.backgroundColor = "white";
+
+            document.getElementById("p1").style.backgroundColor = "black";
+            document.getElementById("p2").style.backgroundColor = "black";
+            document.getElementById("p6").style.backgroundColor = "black";
+            document.getElementById("p7").style.backgroundColor = "black";
+
+            break;
+
+        case 5:
+
+            document.getElementById("p3").style.backgroundColor = "white";
+            document.getElementById("p5").style.backgroundColor = "white";
+
+            document.getElementById("p1").style.backgroundColor = "black";
+            document.getElementById("p2").style.backgroundColor = "black";
+            document.getElementById("p4").style.backgroundColor = "black";
+            document.getElementById("p6").style.backgroundColor = "black";
+            document.getElementById("p7").style.backgroundColor = "black";
+
+            break;
+
+        case 6:
+
+            document.getElementById("p4").style.backgroundColor = "white";
+
+            document.getElementById("p1").style.backgroundColor = "black";
+            document.getElementById("p2").style.backgroundColor = "black";
+            document.getElementById("p3").style.backgroundColor = "black";
+            document.getElementById("p5").style.backgroundColor = "black";
+            document.getElementById("p6").style.backgroundColor = "black";
+            document.getElementById("p7").style.backgroundColor = "black";
+
+            break;
+
+    }
+
 }
