19a84409326c6bf9c8a233aa3d6e0e35843da6a6
Gabriel <g2012ribeiro@gmail.com>
2021-01-29 11:21:32 -0300

    Alteração
    default sincroniza com o browser, build constroi

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,50 @@
-const { parallel, src , dest}    = require('gulp')
-                    ,cssmin      = require('gulp-clean-css')
-                    ,concat      = require('gulp-concat')
-                    ,htmlmin     = require('gulp-htmlmin')
-                    ,htmlreplace = require('gulp-html-replace')
-                    ,inlnsrc     = require('gulp-inline-source');
-                    
-exports.default = parallel(
-    () => {
-        return src("./src/classes/*.css")
-            .pipe(concat("estilos.min.css"))
-            .pipe(cssmin())
-            .pipe(dest("./dist/classes/"));
-    },
-    () => {
-        return src("./src/dados/**/*.css")
-            .pipe(cssmin())
-            .pipe(dest("./dist/dados/"));
-    },
-    () => {
-        return src("./src/**/*.html")
-            .pipe(htmlreplace(
-                {
-                    css:{
-                        src: "classes/estilos.min.css"
-                    },
-                    cssDado:{
-                        src: "../../classes/estilos.min.css"
-                    }
+const { parallel, src , dest, watch} = require('gulp')
+    ,cssmin = require('gulp-clean-css')
+    ,concat = require('gulp-concat')
+    ,htmlmin = require('gulp-htmlmin')
+    ,htmlreplace = require('gulp-html-replace')
+    ,inlnsrc = require('gulp-inline-source')
+    ,browserSync = require('browser-sync').create();
+
+const reload = browserSync.reload;
+
+sincronizaBrowser = () => {
+    browserSync.init({
+        proxy: "localhost:5500"
+    });
+    watch("src/**/*.css").on('change', reload);
+    watch("src/**/*.html").on('change', reload);
+};
+
+minCssClasses = () => {
+    return src("./src/classes/*.css")
+        .pipe(concat("estilos.min.css"))
+        .pipe(cssmin())
+        .pipe(dest("./dist/classes/"));
+}
+
+minCssDados = () => {
+    return src("./src/dados/**/*.css")
+        .pipe(cssmin())
+        .pipe(dest("./dist/dados/"));
+}
+
+buildHTML = () => {
+    return src("./src/**/*.html")
+        .pipe(htmlreplace(
+            {
+                css:{
+                    src: "classes/estilos.min.css"
+                },
+                cssDado:{
+                    src: "../../classes/estilos.min.css"
                 }
-            ))
-            .pipe(inlnsrc())
-            .pipe(htmlmin({ collapseWhitespace: true }))
-            .pipe(dest("./dist/"));
-    }
-);
+            }
+        ))
+        .pipe(inlnsrc())
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest("./dist/"));
+}
+
+exports.default = sincronizaBrowser;
+exports.build = parallel(minCssClasses, minCssDados, buildHTML);
