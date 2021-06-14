de306ce63c80f1937367b501daa5b04fab5d153b
Gabriel <g2012ribeiro@gmail.com>
2021-06-14 19:17:17 -0300

    Inverte cores
    Trocando cores do Principal e Secundaria do Fundo

--- src/Colors.js
+++ src/Colors.js
@@ -1,5 +1,5 @@
 export const corTextoPrincipal  = `white`;
-export const corFundoPrincipal  = `#333`
-export const corFundoSecundaria = `#666`;
+export const corFundoPrincipal  = `#666`;
+export const corFundoSecundaria = `#333`;
 export const corDadoPadrao      = '#222';
 export const coresCirculos      = ['#f66', '#66f', '#6f6', '#f6f', '#6ff', '#ff6'];
