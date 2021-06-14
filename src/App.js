ee613345a03a1b7ce59b4c0303a5c0c9496f1d1e
Gabriel <g2012ribeiro@gmail.com>
2021-06-14 19:28:34 -0300

    Adicionada Descrição

--- src/App.js
+++ src/App.js
@@ -18,7 +18,21 @@ function App() {
         <Route exact path='/dadojs'>
           <Conteudo>
             <CaixaTexto>
-              Um APP de dados
+              Esta é uma aplicação onde estou tentando implementar alguns dados(D4, D6, D20...)
+            </CaixaTexto>
+            <CaixaTexto>
+              Para entrar em contato:
+              <ol>
+                <li>
+                <a href="https://github.com/Tetr4k">GitHub</a>
+                </li>
+                <li>
+                <a href="https://www.linkedin.com/in/gabrielr-dev/">Linkedin</a>
+                </li>
+                <li>
+                <a href="https://twitter.com/ribeir_tk">Twitter</a>
+                </li>
+              </ol>
             </CaixaTexto>
           </Conteudo>
         </Route>
