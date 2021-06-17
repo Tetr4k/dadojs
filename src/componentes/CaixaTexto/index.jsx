8fda5eec65e10a90aae86647e7de8e3121af69e9
Gabriel <g2012ribeiro@gmail.com>
2021-06-17 09:50:09 -0300

    Alteração de estilo
    Adiciona sombra a alguns elementos

--- src/componentes/CaixaTexto/index.jsx
+++ src/componentes/CaixaTexto/index.jsx
@@ -5,6 +5,7 @@ const CaixaTexto = styled.p`
     background: ${corFundoSecundaria};
     border: ${corFundoSecundaria} 1px solid;
     border-radius: 2rem;
+    box-shadow: 0 0.3rem 1rem 0 black;
     color: ${corTextoPrincipal};
     line-height: 2rem;
     margin-bottom: 1rem;
--- src/componentes/Titulo/index.jsx
+++ src/componentes/Titulo/index.jsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components';
-import { corTextoPrincipal } from '../../Colors'
+import { corTextoPrincipal, corTextoSecundaria } from '../../Colors'
 
 const Titulo = styled(Link)`
+  align-items: center;
   color: ${corTextoPrincipal};
   font-size: 3rem;
-  text-decoration: none;
-  align-items: center;
   justify-content: center;
   margin-left: 1rem;
+  text-decoration: none;
+  text-shadow: 0 0 0.5rem black;
+  &:hover{
+    color: ${corTextoSecundaria};
+    text-shadow: none;
+  }
 `
 
 const Retorno = () => {
